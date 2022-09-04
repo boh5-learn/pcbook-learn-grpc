b4f40b6fcc6350b573dbcbfdeadb1496de4678a4
dilless <bohuang1994@gmail.com>
2022-09-05 00:07:38 +0800

    Added serialize protobuf message

---
+++ serializer/json.go
@@ -0,0 +1,17 @@
+package serializer
+
+import (
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+// ProtobufToJSON converts protocol buffer message to JSON string
+func ProtobufToJSON(message proto.Message) (string, error) {
+	marshaller := protojson.MarshalOptions{
+		Indent:          "  ",
+		UseProtoNames:   true,
+		UseEnumNumbers:  false,
+		EmitUnpopulated: true,
+	}
+	return marshaller.Format(message), nil
+}
